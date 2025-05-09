36c35e5a5e37631c589b10979030ffaf2703937f
tatiana263 <142834149+tatiana263@users.noreply.github.com>
2025-05-09 01:09:34 -0500

    Update conteoLetras.java

--- src/co/edu/unbosque/controller/conteoLetras.java
+++ src/co/edu/unbosque/controller/conteoLetras.java
@@ -1,16 +1,12 @@
 package co.edu.unbosque.controller;
 
-import java.util.Scanner;
-
 public class conteoLetras {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Ingrese una palabra: ");
-        String palabra = sc.nextLine();
-
+       
+        String palabra = "Buenas Tardes";  
         int contador = 0;
+
         for (int i = 0; i < palabra.length(); i++) {
             char c = palabra.charAt(i);
             if (Character.isLetter(c)) {
